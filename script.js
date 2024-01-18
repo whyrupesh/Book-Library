3ee5ce8fc4f8ae2521e84af3c4bdf28d95abcf91
whyrupesh <srupeshkumar800@gmail.com>
2024-01-18 10:57:09 +0530

    makes new card on clicking submit button

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ var formBox =document.querySelector(".form-box-inner");
 
 addbook.
 addEventListener("click",function(){
-    if (formBox.style.display === "none") {
-        formBox.style.display = "block";
-      } else {
+    if (formBox.style.display === "block") {
         formBox.style.display = "none";
+      } else {
+        formBox.style.display = "block";
       }
 })
 
@@ -27,13 +27,34 @@ addEventListener("click",function(){
 var card = document.querySelector(".card");
 var submitButton = document.querySelector(".submit-button");
 
-var newH1Content = "Rupesh";
 
 submitButton.addEventListener("click", function(event) {
   event.preventDefault(); 
-  
-  var h1Element = card.querySelector("h1");
-  h1Element.textContent = newH1Content;
+
+  var newDiv = document.createElement("div");
+  var newh1 = document.createElement("h1");
+  var newh2 = document.createElement("h2");
+  var newh3 = document.createElement("h3");
+  var newp = document.createElement('p');
+
+  newDiv.className = 'card';
+
+
+
+
+  newh1.textContent = document.getElementById("name").value;
+  newh2.textContent = "by "+ document.getElementById("author").value;
+  newh3.textContent = document.getElementById("pages").value + " Pages";
+  newp.textContent = document.getElementById("read").value;
+
+
+  newDiv.appendChild(newh1);
+  newDiv.appendChild(newh2);
+  newDiv.appendChild(newh3);
+  newDiv.appendChild(newp);
+
+  allcards.appendChild(newDiv);
+
 });
 
 
